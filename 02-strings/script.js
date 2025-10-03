e7df478c1c8b87a3cfb438720aa9ded1d7bb5b7a
ousemaakacha <akachaousema@gmail.com>
2025-10-03 13:31:52 +0200

    ex 2 completition

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,11 @@ console.log(myName);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number = 5;
+let text = "java";
+let bool = true;
+console.log(`${number} ${text} ${bool}`);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente
 // in console la stringa con il nome completo
